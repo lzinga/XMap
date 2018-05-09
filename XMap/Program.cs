fd823614c7010db2f69f9b8ee465adfbeb38f20e
Lucas Elzinga <lucas.elzinga@outlook.com>
2018-05-08 19:49:43 -0700

    Add project files.

---
+++ XMap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
